e0cbb15b74f4141e7a44af40b764855c83ced75b
Srujan Bethi <ssbethi@gmail.com>
2020-10-26 17:21:56 -0600

    Cleaned up

--- siteConfig.js
+++ siteConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: 'Srujan Bethi',
-  shortName: 'LAM',
-  description: 'A Full-Stack JavaScript Software Engineer with a knack for solving complex problems, a keen eye for detail, and a passion for learning new technologies',
+  shortName: 'SSB',
+  description: 'A Full-Stack JavaScript Software Engineer with an ability to solve complex problems, a keen eye for detail, and a passion for learning new technologies.',
   url: 'https://gatsby-lam.vaporwavy.io', // Domain of your site without prefix!
   prefix: '/',
   author: 'Srujan Bethi',
