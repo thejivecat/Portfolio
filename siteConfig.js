3f81dae8832633c993be183f3491ed8ebd9fcf0c
Srujan Bethi <ssbethi@gmail.com>
2020-10-26 16:11:11 -0600

    Added some photos and modified about page

--- siteConfig.js
+++ siteConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: 'Srujan Bethi',
   shortName: 'LAM',
-  description: 'A Full-Stack JavaScript developer with a knack for solving complex problems, a keen eye for detail, and a passion for learning new technologies',
+  description: 'A Full-Stack JavaScript Software Engineer with a knack for solving complex problems, a keen eye for detail, and a passion for learning new technologies',
   url: 'https://gatsby-lam.vaporwavy.io', // Domain of your site without prefix!
   prefix: '/',
   author: 'Srujan Bethi',
--- src/pages/about.js
+++ src/pages/about.js
@@ -18,35 +18,50 @@ const AboutPage = ({ data }, location) => {
       <article className="post-content page-template no-image">
         <div className="post-content-body">
           <h2 id="clean-minimal-and-deeply-customisable-london-is-a-theme-made-for-people-who-appreciate-simple-lines-">
-            Hello! My name is Srujan Bethi, and I'm a Full-Stack JavaScript developer based in Boulder, Colorado.
+            Hello! My name is Srujan Bethi, and I'm a Full-Stack JavaScript Software Engineer based in Boulder, Colorado.
           </h2>
-          <figure className="kg-card kg-image-card kg-width-full">
+          {/* <figure className="kg-card kg-image-card kg-width-full">
             <Img
               fluid={data.benchAccounting.childImageSharp.fluid}
               className="kg-image"
             />
             <figcaption>Large imagery is at the heart of this theme</figcaption>
-          </figure>
-          <h3 id="dynamic-styles">Dynamic styles</h3>
+          </figure> */}
+          <h3 id="dynamic-styles">Technologies</h3>
           <p>
-            London comes with photo-centric main layout best suited to
-            photography, graphics portfolios and other image-heavy uses.
+            My programming language of choice is JavaScript for its flexibility, widespread use in web development,
+            and simplicity. I use JS, HTML/CSS, and React for my Frontend work, and Node/Express for Backend work. 
+            I love being able to build complete Full-Stack applications with only one programming language. I'm 
+            currently working on learning React Native for mobile development. I'm also working on learning Python
+            as my second language.
           </p>
           <p>
-            Both post and page templates are light and minimal, with all the
-            focus on the content while the design of the theme gets out of the
-            way. Beneath the hood, London enjoys the full power of the{" "}
-            <a href="https://docs.ghost.org/api/handlebars-themes/">
-              Ghost Handlebars Theme API
-            </a>{" "}
-            to provide limitless customisation options and dynamic styles.
-          </p>
-          <p>
-            Don't forget to check out the{" "}
-            <a href="https://docs.ghost.org/integrations/">
-              Ghost Integrations Directory
-            </a>{" "}
-            for more ways to integrate Ghost with your favourite services.
+            <h4>Complete list of technologies I've worked with: </h4>
+            <ul>
+              <li><a href="https://developer.mozilla.org/en-US/docs/Web/javascript" target="_blank">JavaScript</a></li>
+              <li><a href="https://www.typescriptlang.org/docs/" target="_blank">TypeScript</a></li>
+              <li><a href="https://developer.mozilla.org/en-US/docs/Web/HTML" target="_blank">HTML</a></li>
+              <li><a href="https://css-tricks.com/" target="_blank">CSS</a></li>
+              <li><a href="https://reactjs.org/" target="_blank">React</a></li>
+              <li><a href="https://redux.js.org/" target="_blank">Redux</a></li>
+              <li><a href="https://nodejs.org/en/docs/" target="_blank">Node</a></li>
+              <li><a href="https://expressjs.com/" target="_blank">Express</a></li>
+              <li><a href="https://www.w3schools.com/sql/sql_quickref.asp" target="_blank">SQL</a></li>
+              <li><a href="https://dev.mysql.com/doc/" target="_blank">MySQL</a></li>
+              <li><a href="https://www.postgresql.org/" target="_blank">PostgreSQL</a></li>
+              <li><a href="https://www.mongodb.com/" target="_blank">MongoDB</a></li>
+              <li><a href="https://redis.io/documentation" target="_blank">Redis</a></li>
+              <li><a href="https://docs.github.com/en" target="_blank">Git</a></li>
+              <li><a href="https://docs.docker.com/" target="_blank">Docker</a></li>
+              <li><a href="https://restfulapi.net/" target="_blank">REST</a></li>
+              <li><a href="https://circleci.com/" target="_blank">CircleCI</a></li>
+              <li><a href="https://docs.aws.amazon.com/index.html?nc2=h_ql_doc_do" target="_blank">Amazon Web Services</a></li>
+              <li><a href="https://sass-lang.com/documentation" target="_blank">SASS(CSS)</a></li>
+              <li><a href="https://styled-components.com/docs" target="_blank">Styled Components</a></li>
+              <li><a href="https://api.jquery.com/" target="_blank">jQuery</a></li>
+              <li><a href="https://jestjs.io/docs/en/getting-started" target="_blank">Jest(React Testing Suite)</a></li>
+              <li><a href="https://enzymejs.github.io/enzyme/" target="_blank">Enzyme(React Testing Suite)</a></li>
+            </ul>
           </p>
         </div>
       </article>
